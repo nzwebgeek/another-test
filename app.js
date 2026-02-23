04b16ab516c74620f00132aced0532484b0f7f68
mikenzwebgeek <mikenzwebgeek@gmail.com>
2026-02-23 19:26:45 +1300

    add changes to index and js file

--- app.js
+++ app.js
@@ -16,6 +16,6 @@
 
 
         result.textContent = `You belong to: ${generation}`;
-        pic.innerHTML = `<img src="https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fsixtygram.com%2Fsixtygram_content%2Fuploads%2F2025%2F01%2FGeneration_timeline.svg_-2048x1280.png&f=1&nofb=1&ipt=7369aff5f9d04d0a2cef7760b9c23809e298afe9d455b031048f14b6301d0300" width="100%" alt="${generation}">`;
+        pic.innerHTML = `<img src="generations.png" width="100%" alt="${generation}">`;
         form.reset();
     });
